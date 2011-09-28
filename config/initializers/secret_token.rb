05be32bcffa066b69a5d228773652c0cb0bdcb19
Mac Van Anh <lamhaianh@gmail.com>
2011-09-28 09:09:06 +0700

    Create Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VCom::Application.config.secret_token = '90d0efe8c95c90f2b228544d497dac2d0caa04994f48b4701a6518a51dce01606958921f213c9959374b527e39801471f02cb392083da575c08f590ec1f9cc4a'
